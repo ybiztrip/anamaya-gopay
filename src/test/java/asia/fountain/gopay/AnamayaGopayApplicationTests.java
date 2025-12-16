5f35d550393cb5a2cf07b1118669929fdf30c96f
yogi <yogi@anchorock.com>
2025-12-16 16:32:44 +0700

    Initial commit

---
+++ src/test/java/asia/fountain/gopay/AnamayaGopayApplicationTests.java
@@ -0,0 +1,13 @@
+package asia.fountain.gopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnamayaGopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
